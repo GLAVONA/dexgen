219f9471b7f1eb59ba584c9e9429474bdaa9585c
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2023-03-03 17:46:35 +0200

    updated navbar on non-wallet version

--- src/App-nowallet.js
+++ src/App-nowallet.js
@@ -15,49 +15,71 @@ import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { avalanche, avalancheFuji, localhost } from "wagmi/chains";
 import { publicProvider } from "wagmi/providers/public";
 import { Route, Routes } from "react-router";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import ConnectButtonHOC from "./components/ConnectButtonHOC";
 import { CustomConnect } from "./components/CustomConnect";
 
 const AppNoWallet = () => {
   const [mode, setMode] = useState("swap");
-  
+
   return (
-        <div className="App">
-          <Navbar>
-            <div id="logo">DEXGEN</div>
-            <div id="connect-no-wallet">
-            Please install a wallet
-            </div>
-          </Navbar>
-          <div id="choose-mode">
-            <div className="wrapper">
-              <Link to="/">
-                <div
-                  className={`swap-mode ${mode === "swap" ? "active" : null}`}
-                  onClick={() => setMode("swap")}
-                >
-                  Swap
-                </div>
-              </Link>
-              <Link to="/liquidity">
-                <div
-                  className={`liq-mode ${mode === "liq" ? "active" : null}`}
-                  onClick={() => setMode("liq")}
-                >
-                  Liquidity
-                </div>
-              </Link>
-            </div>
-          </div>
-          <div id="main-bg"></div>
-          <div id="main">
-            <Routes>
-              <Route path="/" element={<Swap setMode={setMode}/>} />
-              <Route path="/liquidity" element={<Liquidity setMode={setMode}/>} />
-            </Routes>
-          </div>
+    <div className="App">
+      <Navbar>
+        <div className="left">
+          <div id="logo">DEXGEN</div>
         </div>
+        <ul className="nav-menu">
+          <li>
+            {" "}
+            <NavLink to="/" className={"nav-item"}>
+              Trade
+            </NavLink>{" "}
+          </li>
+          <li>
+            {" "}
+            <NavLink to="/stake" className={"nav-item"}>
+              Stake
+            </NavLink>{" "}
+          </li>
+          <li>
+            {" "}
+            <NavLink to="/farm" className={"nav-item"}>
+              Farm
+            </NavLink>{" "}
+          </li>
+        </ul>
+        <div className="right">
+          <div id="connect-no-wallet">Please install a wallet</div>
+        </div>
+      </Navbar>
+      <div id="choose-mode">
+        <div className="wrapper">
+          <Link to="/">
+            <div
+              className={`swap-mode ${mode === "swap" ? "active" : null}`}
+              onClick={() => setMode("swap")}
+            >
+              Swap
+            </div>
+          </Link>
+          <Link to="/liquidity">
+            <div
+              className={`liq-mode ${mode === "liq" ? "active" : null}`}
+              onClick={() => setMode("liq")}
+            >
+              Liquidity
+            </div>
+          </Link>
+        </div>
+      </div>
+      <div id="main-bg"></div>
+      <div id="main">
+        <Routes>
+          <Route path="/" element={<Swap setMode={setMode} />} />
+          <Route path="/liquidity" element={<Liquidity setMode={setMode} />} />
+        </Routes>
+      </div>
+    </div>
   );
 };
 export default AppNoWallet;
